f1a0f40f699c1521e3d2d5c9872ad61d8aa7db5c
jjs9000 <farriszamrie15@gmail.com>
2024-05-21 07:14:15 +0800

    fixed logo route for admin & user in app layout

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -21,12 +21,20 @@ new class extends Component
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+                @if (Auth::check() && Auth::user()->usertype === 'user')
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard.index') }}" wire:navigate>
+                    <a href="{{ route('task-posting.index') }}" wire:navigate>
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 " />
                     </a>
                 </div>
+                @else
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('admin-dashboard') }}" wire:navigate>
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 " />
+                    </a>
+                </div>
+                @endif
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
