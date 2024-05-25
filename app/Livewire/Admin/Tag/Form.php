af0354e45ef32d4e4559cfa3ec36489db0505c38
jjs9000 <farriszamrie15@gmail.com>
2024-05-25 20:02:42 +0800

    added validation for admin add tag form

--- app/Livewire/Admin/Tag/Form.php
+++ app/Livewire/Admin/Tag/Form.php
@@ -10,14 +10,23 @@ class Form extends Component
     public $name;
     public $description;
 
-    protected $rules = [
-        'name' => 'required|string|max:255',
-        'description' => 'nullable|string|max:255',
+    protected $messages = [
+        'name.required' => 'The tag name is required.',
+        'name.string' => 'The tag name must be in type string.',
+        'name.max' => 'The tag name is too long. Maximum 15 characters',
+
+        'description.string' => 'The tag description must be in type string.',
+        'description.max' => 'The tag description is too long. Maximum 50 characters.',
     ];
 
     public function submit()
     {
-        $this->validate();
+        $rules = [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
+        ];
+
+        $this->validate($rules);
 
         Tag::create([
             'name' => $this->name,
--- resources/views/livewire/admin/tag/form.blade.php
+++ resources/views/livewire/admin/tag/form.blade.php
@@ -8,18 +8,16 @@
     <form wire:submit.prevent="submit">
         <div class="mb-4">
             <label for="name" class="block text-gray-700">Name</label>
-            <input type="text" id="name" wire:model="name" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 @error('name') border-red-500 @enderror">
-            @error('name') <span class="text-red-500">{{ $message }}</span> @enderror
+            <x-text-input type="text" id="name" wire:model="name" name="name" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
         </div>
 
         <div class="mb-4">
             <label for="description" class="block text-gray-700">Description</label>
-            <input type="text" id="description" wire:model="description" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 @error('description') border-red-500 @enderror">
-            @error('description') <span class="text-red-500">{{ $message }}</span> @enderror
+            <x-text-input type="text" id="description" wire:model="description" name="description" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
         </div>
 
-        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+        <x-primary-button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
             Add Tag
-        </button>
+        </x-primary-button>
     </form>
 </div>
