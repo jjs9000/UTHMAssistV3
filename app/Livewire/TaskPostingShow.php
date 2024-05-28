33ee03c199b139edb717639b4ef683a85a2d5287
jjs9000 <farriszamrie15@gmail.com>
2024-05-28 17:51:05 +0800

    added necessary data to display in TaskPostingShow

--- app/Livewire/TaskPostingShow.php
+++ app/Livewire/TaskPostingShow.php
@@ -2,22 +2,51 @@
 
 namespace App\Livewire;
 
+use App\Models\Bookmark;
 use Livewire\Component;
 use App\Models\TaskPosting;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\On;
 
 class TaskPostingShow extends Component
 {
     public $task;
+    public $savedTask;
+    public $savedTaskIds = [];
 
     public function mount($id)
     {
         $this->task = TaskPosting::findOrFail($id);
     }
 
+    public function toggleBookmark($taskId)
+    {
+        $this->savedTask = TaskPosting::find($taskId);
+
+        if (in_array($taskId, $this->savedTaskIds)) {
+            Bookmark::where('user_id', Auth::id())
+                    ->where('task_posting_id', $taskId)
+                    ->delete();
+            $this->savedTaskIds = array_diff($this->savedTaskIds, [$taskId]);
+        } else {
+            Bookmark::create([
+                'user_id' => Auth::id(),
+                'task_posting_id' => $taskId,
+            ]);
+            $this->savedTaskIds[] = $taskId;
+        }
+    }
+
     public function render()
     {
-        return view('livewire.task-posting-show');
+        $this->savedTaskIds = Bookmark::where('user_id', Auth::id())
+                                           ->pluck('task_posting_id')
+                                           ->toArray();
+                                           
+        return view('livewire.task-posting-show',[
+            'savedTask' => $this->savedTask,
+            'savedTaskIds' => $this->savedTaskIds,
+        ]);
     }
 
     #[On('apply')]
@@ -25,7 +54,7 @@ class TaskPostingShow extends Component
     {
         // Redirect the user to the ApplicationCreateForm component
         return redirect()->route('application-create-form', [
-            'taskId' => $this->task->id
+            'taskId' => $this->task->id,
         ]);
     }
 }
--- resources/views/livewire/task-posting-show.blade.php
+++ resources/views/livewire/task-posting-show.blade.php
@@ -5,20 +5,40 @@
         <p class="text-gray-700">Posted {{ $task->created_at->diffForHumans() }}</p>
     </div>
 
+    <div class="flex flex-wrap mb-4 tag-container">
+        @foreach ($task->tags as $tag)
+            <span class="inline-block bg-gray-900 text-white px-2 py-1 rounded-full text-xs font-semibold mr-2 mb-2">{{ $tag->name }}</span>
+        @endforeach
+    </div>
+
     <!-- Task information -->
     <div class="mb-4">
         <div class="flex items-center mb-2">
             <img src="{{ asset('svg/salary-icon.svg') }}" alt="Salary Icon" class="h-6 w-6 mr-2">
-            <p class="text-gray-700">{{ $task->salary }}</p>
+            <p class="text-gray-700">RM {{ $task->salary }}</p>
         </div>
-        <div class="flex items-center">
+        <div class="flex items-center mb-2">
             <img src="{{ asset('svg/location-icon.svg') }}" alt="Location Icon" class="h-6 w-6 mr-2">
             <p class="text-gray-700">{{ $task->location }}</p>
+        </div>
+        <div class="flex items-center mb-2">
+            <img src="{{ asset('svg/user-icon.svg') }}" alt="Location Icon" class="h-6 w-6 mr-2">
+            <p class="text-gray-700">{{ $task->user->username }}</p>
+        </div>
+        <div class="flex items-center mb-2">
+            <img src="{{ asset('svg/time-icon.svg') }}" alt="Location Icon" class="h-6 w-6 mr-2">
+            <p class="text-gray-700">{{ $task->availability }}</p>
+        </div>
+
+        <div class="flex items-center mt-4">
+            <p class="text-gray-700">
+                <strong>Deadline:</strong> {{ \Carbon\Carbon::parse($task->deadline)->diffForHumans() }}
+            </p>
         </div>
     </div>    
     
     <!-- Apply Button and Bookmark Button -->
-    <div class="flex justify-between items-center mb-4">
+    <div class="flex justify-start items-center space-x-4 mb-4">
         @if (auth()->check())
             @if (!$task->isExpired())
                 @php
@@ -36,10 +56,14 @@
             @endif
         @endif
                 
-        <!-- Bookmark Button -->
-        <button wire:click="bookmark" class="flex items-center justify-center bg-transparent hover:bg-gray-200 text-gray-600 px-4 py-2 rounded-full">
-            <img src="{{ asset('svg/save-icon.svg') }}" alt="Bookmark Icon" class="w-6 h-6 mr-2">
-            <span>Save</span>
+        <button wire:click="toggleBookmark({{ $task->id }})" class="flex items-center justify-center bg-transparent hover:bg-gray-200 text-gray-600 px-4 py-2 rounded-full">
+            @if(in_array($task->id, $savedTaskIds))
+                <img src="{{ asset('svg/save-icon-filled.svg') }}" alt="Saved Icon" class="w-6 h-6 hover:cursor-pointer transition duration-300 ease-in-out transform hover:scale-110">
+                <span>Saved</span>
+            @else
+                <img src="{{ asset('svg/save-icon.svg') }}" alt="Save Icon" class="w-6 h-6 hover:cursor-pointer transition duration-300 ease-in-out transform hover:scale-110">
+                <span>Save</span>
+            @endif
         </button>
     </div>
 
