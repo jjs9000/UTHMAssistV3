2472b1da59f04f64be0dcaf0fdb9f2564d626e29
jjs9000 <farriszamrie15@gmail.com>
2024-06-16 04:29:14 +0800

    added new styling for login and forgot-password page, added responsive design for both

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,15 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FAF9F6] ">
+        {{-- <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FAF9F6] "> --}}
+            <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-image: url('https://wallpapers.com/images/featured/paper-texture-background-zlvqcgwohwvd642j.webp'); background-repeat: no-repeat; background-size: cover;">
             <div>
                 <a href="/" wire:navigate>
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-fit mt-6 px-6 py-4 bg-white shadow-xl overflow-hidden sm:rounded-3xl">
+            <div class="w-full px-6 overflow-hidden">
                 {{ $slot }}
             </div>
         </div>
