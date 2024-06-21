43e536f910459b15b02980b2fd14164a0c1502e5
jjs9000 <farriszamrie15@gmail.com>
2024-06-22 03:24:48 +0800

    added responsive design to the user dashboard

--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -1,28 +1,28 @@
-<div>
-    <div>
-        <nav class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
-            <button wire:click="$dispatch('showUserProfile')" class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 transition-colors duration-300 ease-in-out hover:bg-indigo-100 focus:outline-none focus:bg-indigo-100 @if($showUserProfile) bg-indigo-100 @endif">
-                <img src="{{ asset('svg/user-icon-filled.svg') }}" alt="Profile Icon" class="w-6 h-6 shrink-0">
-                <small class="text-center text-xs font-medium">Profile</small>
-            </button>
-            <button wire:click="$dispatch('showUserTask')" class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 transition-colors duration-300 ease-in-out hover:bg-indigo-100 focus:outline-none focus:bg-indigo-100 @if($showUserTask) bg-indigo-100 @endif">
-                <img src="{{ asset('svg/task-icon.svg') }}" alt="Task Icon" class="w-6 h-6 shrink-0">
-                <small class="text-center text-xs font-medium">Task</small>
-            </button>
-            <button wire:click="$dispatch('showUserTimetable')" class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 transition-colors duration-300 ease-in-out hover:bg-indigo-100 focus:outline-none focus:bg-indigo-100 @if($showUserTimetable) bg-indigo-100 @endif">
-                <img src="{{ asset('svg/table-icon.svg') }}" alt="Timetable Icon" class="w-6 h-6 shrink-0">
-                <small class="text-center text-xs font-medium">Timetable</small>
-            </button>
-        </nav>
+<div class="flex flex-col min-h-screen">
+    <div class="flex-grow pb-16"> <!-- Added padding-bottom to provide space for the bottom navigation -->
+        <div class="mt-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            @if($showUserProfile)
+                <livewire:dashboard.profile.index />
+            @elseif($showUserTask)
+                <livewire:dashboard.task.index />
+            @elseif($showUserTimetable)
+                <livewire:dashboard.timetable.timetable-manager />
+            @endif
+        </div>
     </div>
 
-    <div class="mt-10 ml-28">
-        @if($showUserProfile)
-            <livewire:dashboard.profile.index />
-        @elseif($showUserTask)
-            <livewire:dashboard.task.index />
-        @elseif($showUserTimetable)
-            <livewire:dashboard.timetable.timetable-manager />
-        @endif
-    </div>
+    <nav class="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-xs bg-white/50 border-t border-gray-200 shadow-lg backdrop-blur-lg p-2.5 flex justify-around gap-4 rounded-lg border">
+        <button wire:click="$dispatch('showUserProfile')" class="flex items-center justify-center flex-col gap-1 min-h-[32px] w-16 rounded-md p-1.5 transition-colors duration-300 ease-in-out hover:bg-indigo-100 focus:outline-none focus:bg-indigo-100 @if($showUserProfile) bg-indigo-100 @endif">
+            <img src="{{ asset('svg/user-icon-filled.svg') }}" alt="Profile Icon" class="w-6 h-6">
+            <small class="text-xs font-medium">Profile</small>
+        </button>
+        <button wire:click="$dispatch('showUserTask')" class="flex items-center justify-center flex-col gap-1 min-h-[32px] w-16 rounded-md p-1.5 transition-colors duration-300 ease-in-out hover:bg-indigo-100 focus:outline-none focus:bg-indigo-100 @if($showUserTask) bg-indigo-100 @endif">
+            <img src="{{ asset('svg/task-icon.svg') }}" alt="Task Icon" class="w-6 h-6">
+            <small class="text-xs font-medium">Task</small>
+        </button>
+        <button wire:click="$dispatch('showUserTimetable')" class="flex items-center justify-center flex-col gap-1 min-h-[32px] w-16 rounded-md p-1.5 transition-colors duration-300 ease-in-out hover:bg-indigo-100 focus:outline-none focus:bg-indigo-100 @if($showUserTimetable) bg-indigo-100 @endif">
+            <img src="{{ asset('svg/table-icon.svg') }}" alt="Timetable Icon" class="w-6 h-6">
+            <small class="text-xs font-medium">Timetable</small>
+        </button>
+    </nav>
 </div>
--- resources/views/livewire/dashboard/task/completed-task.blade.php
+++ resources/views/livewire/dashboard/task/completed-task.blade.php
@@ -1,5 +1,5 @@
 <div class="py-12">
-    <div class="mt-8 max-w-7xl mx-auto">
+    <div class="mt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 class="text-2xl text-gray-900 font-semibold mb-4">Completed Tasks</h1>
 
         <!-- Sort By -->
@@ -13,8 +13,14 @@
             </div>
         </div>
 
+        @if ($applications->isEmpty())
+        <div class="p-6 text-center">
+            <p class="text-lg font-semibold text-gray-700">No completed tasks</p>
+        </div>
+        @else
         <div class="overflow-x-auto">
-            <table class="min-w-full shadow-md">
+            <!-- Table for larger screens -->
+            <table class="min-w-full shadow-md table-auto w-full hidden sm:table">
                 <thead class="bg-white border-2 border-gray-900">
                     <tr>
                         <th class="py-3 px-6 text-left text-md font-semibold text-gray-700">Task</th>
@@ -24,41 +30,68 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white border-2 border-gray-900 divide-y divide-gray-900">
-                    @forelse ($applications as $application)
-                        <tr>
-                            <td class="py-4 px-6 text-sm text-gray-900">{{ $application->task->title }}</td>
-                            <td class="py-4 px-6 text-sm text-gray-900">{{ $application->user->username }}</td>
-                            <td class="py-4 px-6 text-sm text-gray-900">{{ $application->task->status }}</td>
-                            <td class="py-4 px-6 text-sm">
-                                @if (auth()->id() === $application->task->user_id)
-                                <div>
-                                    <button wire:click="$dispatch('openModal', { component: 'dashboard.task.modal.view-task-rating-modal', arguments: { taskId: {{ $application->task->id }} } })">
-                                        <img src="{{ asset('svg/view-icon.svg') }}" alt="View Reviews" class="w-6 h-6">
-                                    </button>
-                                </div>
-                                @elseif (auth()->id() === $application->user_id)
-                                <!-- Applicant can rate the task -->
-                                @php
-                                    $userFeedback = \App\Models\Feedback::where('task_id', $application->task_id)
-                                        ->where('user_id', auth()->id())
-                                        ->first();
-                                @endphp
-                                @if ($userFeedback)
-                                    <button class="bg-gray-500 text-white px-4 py-2 rounded" disabled>Rated</button>
-                                @else
-                                    <button wire:click="$dispatch('openModal', { component: 'dashboard.task.modal.task-rating-modal', arguments: { taskId: {{ $application->task->id }} } })" class="bg-blue-500 text-white px-4 py-2 rounded">Rate Task</button>
-                                @endif
+                    @foreach ($applications as $application)
+                    <tr>
+                        <td class="py-4 px-6 text-sm text-gray-900">{{ $application->task->title }}</td>
+                        <td class="py-4 px-6 text-sm text-gray-900">{{ $application->user->username }}</td>
+                        <td class="py-4 px-6 text-sm text-gray-900">{{ $application->task->status }}</td>
+                        <td class="py-4 px-6 text-sm">
+                            @if (auth()->id() === $application->task->user_id)
+                            <div>
+                                <button wire:click="$dispatch('openModal', { component: 'dashboard.task.modal.view-task-rating-modal', arguments: { taskId: {{ $application->task->id }} } })">
+                                    <img src="{{ asset('svg/view-icon.svg') }}" alt="View Reviews" class="w-6 h-6">
+                                </button>
+                            </div>
+                            @elseif (auth()->id() === $application->user_id)
+                            <!-- Applicant can rate the task -->
+                            @php
+                                $userFeedback = \App\Models\Feedback::where('task_id', $application->task_id)
+                                    ->where('user_id', auth()->id())
+                                    ->first();
+                            @endphp
+                            @if ($userFeedback)
+                            <button class="bg-gray-500 text-white px-4 py-2 rounded" disabled>Rated</button>
+                            @else
+                            <button wire:click="$dispatch('openModal', { component: 'dashboard.task.modal.task-rating-modal', arguments: { taskId: {{ $application->task->id }} } })" class="bg-blue-500 text-white px-4 py-2 rounded">Rate Task</button>
                             @endif
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="py-4 px-6 text-center text-gray-900 font-semibold text-lg">No completed tasks</td>
-                        </tr>
-                    @endforelse
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
                 </tbody>
             </table>
+
+            <!-- Cards for mobile view -->
+            <div class="sm:hidden space-y-4">
+                @foreach ($applications as $application)
+                <div class="bg-white shadow-md rounded-lg p-4 border-2 border-gray-900">
+                    <h2 class="text-lg font-semibold text-gray-900">{{ $application->task->title }}</h2>
+                    <p class="mt-2 text-sm text-gray-900">{{ $application->user->username }}</p>
+                    <p class="mt-2 text-sm text-gray-900">Status: {{ $application->task->status }}</p>
+                    <div class="mt-4 flex space-x-2">
+                        @if (auth()->id() === $application->task->user_id)
+                        <button wire:click="$dispatch('openModal', { component: 'dashboard.task.modal.view-task-rating-modal', arguments: { taskId: {{ $application->task->id }} } })">
+                            <img src="{{ asset('svg/view-icon.svg') }}" alt="View Reviews" class="w-6 h-6">
+                        </button>
+                        @elseif (auth()->id() === $application->user_id)
+                        <!-- Applicant can rate the task -->
+                        @php
+                            $userFeedback = \App\Models\Feedback::where('task_id', $application->task_id)
+                                ->where('user_id', auth()->id())
+                                ->first();
+                        @endphp
+                        @if ($userFeedback)
+                        <button class="bg-gray-500 text-white px-4 py-2 rounded" disabled>Rated</button>
+                        @else
+                        <button wire:click="$dispatch('openModal', { component: 'dashboard.task.modal.task-rating-modal', arguments: { taskId: {{ $application->task->id }} } })" class="bg-blue-500 text-white px-4 py-2 rounded">Rate Task</button>
+                        @endif
+                        @endif
+                    </div>
+                </div>
+                @endforeach
+            </div>
         </div>
+        @endif
 
         <!-- Pagination -->
         <div class="mt-4">
